6c643bc9b2f61cba3303eecb2bc0fc90f7f6217b
Prasad <prasadmhaske2001@gmail.com>
2023-09-27 23:05:25 +0530

    userId added in cart

--- models/cart.model.js
+++ models/cart.model.js
@@ -2,9 +2,10 @@ const { Schema, model } = require("mongoose");
 
 const cartSchema = new Schema({
   _id: { type: Schema.Types.ObjectId, ref: "product", required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "user", required: true },
   name: { type: String, required: true },
   rating: { type: Number, required: true },
-  discount: { type:Number, required: true},
+  discount: { type: Number, required: true },
   price: { type: Number, required: true },
   type: { type: String, required: true },
   category: { type: String, required: true },
